b2028b41989d3b1aff481844c233f87a6713c1f4
Anwooseong <aws5624@naver.com>
2022-03-14 03:49:19 +0900

    최댓값(기본)

---
+++ 백준2562.py
@@ -0,0 +1,15 @@
+import sys
+
+
+input = sys.stdin.readline
+num_list = [0]*9
+max_index = 0
+max_value = 0
+for i in range(len(num_list)):
+    num_list[i] = int(input())
+    if max_value < num_list[i]:
+        max_index = i+1
+        max_value = num_list[i]
+print(max_index)
+print(max_value)
+        
